29bf49ca6ed3514d82065beee48c1f11ed8d5312
snehasunil-25 <snehasoul2005@gmail.com>
2025-06-08 11:24:28 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 100) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+//Profile section
+//redirection
+const profileIcon = document.querySelector(".profile-icon");
+profileIcon.addEventListener("click", () => {
+  window.location.href = "profile-details.html";
+});
+
+//movie card
+const movieCard = document.querySelectorAll(".content-card");
+movieCard.forEach((card) => {
+  card.addEventListener("mouseEnter", () => {
+    card.style.transform = "scale(1.05)";
+  });
+
+  card.addEventListener("mouseleave", () => {
+    card.style.transform = "scale(1)";
+  });
+});
